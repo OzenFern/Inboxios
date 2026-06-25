b0947a82f341fd31c905ce10714026c1b505731a
OzenFern <ozenfernandes@gmail.com>
2026-06-25 12:25:24 +0530

    Complete task mapper

--- utils/taskMapper.js
+++ utils/taskMapper.js
@@ -1,9 +1,75 @@
-export function mapTask(page) {
-  const due = page.properties.Due;
+export function mapTask(task) {
+  const due = task.properties.Due.date;
+  const status = task.properties.Status?.status?.name;
+  if (!status) throw new Error("Task is missing a Status property");
+
   return {
-    id: page.id,
-    title: page.properties.Name.title[0].text.content,
-    status: page.properties.Status.status.name,
-    due_date: due.date?.end ?? due.date.start ?? null, // Use end date if available, otherwise fallback to start date then null
+    id: task.id,
+    title: task.properties.Name.title[0]?.text?.content ?? "",
+    status,
+    startDate: due?.start ?? null,
+    endDate: due?.end ?? null,
   };
+}
+
+/**
+ * Set date to null if startDate is null or undefined
+ */
+function buildDate(task) {
+  if (task.startDate == undefined) return { date: null };
+  return {
+    date: {
+      start: task.startDate,
+      end: task.endDate,
+    },
+  };
+}
+
+export function toNotionTask(task) {
+  return {
+    properties: {
+      Name: {
+        title: [
+          {
+            text: {
+              content: task.title,
+            },
+          },
+        ],
+      },
+      Status: {
+        status: {
+          name: task.status,
+        },
+      },
+      Due: buildDate(task),
+    },
+  };
+}
+
+export function toNotionUpdate(task) {
+  const properties = {};
+  if (task.title !== undefined) {
+    properties.Name = {
+      title: [
+        {
+          text: {
+            content: task.title,
+          },
+        },
+      ],
+    };
+  }
+
+  if (task.status !== undefined) {
+    properties.Status = {
+      status: {
+        name: task.status,
+      },
+    };
+  }
+  if (task.startDate !== undefined) {
+    properties.Due = buildDate(task);
+  }
+  return { properties };
 }
