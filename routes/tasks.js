918ae13ff86d572a3aea905a119c59ae2ee5c09c
OzenFern <ozenfernandes@gmail.com>
2026-06-25 17:14:42 +0530

    Handle adding new tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,15 @@
 import express from "express";
-import * as notion from "./services/notion.js";
+import * as notion from "../services/taskService.js";
 
 const router = express.Router();
 
 router.get("/", notion.getTasks);
-router.post("/", notion.createTask);
+router.post("/", async (req, res) => {
+  const task = req.body;
+  const response = await notion.createTask(task);
+
+  res.send(response); // TODO: Send a proper response to frontend
+});
 router.patch("/:id", notion.updateTask);
 router.delete("/:id", notion.deleteTask);
 
---
+++ services/taskService.js
@@ -0,0 +1,34 @@
+import { notion } from "../config/notion.js";
+import { mapTask, toNotionTask, toNotionUpdate } from "../utils/taskMapper.js";
+
+function showError(error) {
+  console.error(error.response?.data);
+  throw error;
+}
+
+function createBody(task) {
+  return {
+    parent: {
+      type: "data_source_id",
+      data_source_id: process.env.NOTION_DS_ID,
+    },
+    template: {
+      type: "default",
+    },
+    ...toNotionTask(task),
+  };
+}
+
+export async function getTasks() {}
+export async function createTask(task) {
+  try {
+    task.status ??= "To Do"; // Defaults to To Do if status is missing
+    const body = createBody(task);
+    const response = await notion.post("/pages", body);
+    return mapTask(response.data);
+  } catch (error) {
+    showError(error);
+  }
+}
+export async function updateTask(id) {}
+export async function deleteTask(id) {}
