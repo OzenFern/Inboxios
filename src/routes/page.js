53ed1a8c332d05b7c6a3e94fbab944488a6e02a6
OzenFern <ozenfernandes@gmail.com>
2026-06-26 23:41:49 +0530

    add route for pages

---
+++ src/routes/page.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+router.get("/about", (req, res) => {
+  res.send("About");
+});
+
+export default router;
