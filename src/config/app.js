d8ea60ec6541a71cf808ab10c5bd1da34d6dac60
OzenFern <ozenfernandes@gmail.com>
2026-06-29 00:36:46 +0530

    Add issues link to app config

--- src/config/app.js
+++ src/config/app.js
@@ -4,6 +4,7 @@ export const APP = {
   LINKS: {
     GITHUB: "https://github.com/OzenFern/Inboxios",
     DOCS: "https://github.com/OzenFern/Inboxios#readme",
+    ISSUES: "https://github.com/OzenFern/Inboxios/issues",
     TEMPLATE:
       "https://ozenf.notion.site/cd94f0d949c1834aab81810764b9cd3c?v=0154f0d949c18313b1be08e4a4d3c1c7",
   },
