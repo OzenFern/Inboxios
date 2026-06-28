6d5672a92a0f1c4d23c8fb409bce4fc6d5b59553
OzenFern <ozenfernandes@gmail.com>
2026-06-28 15:03:42 +0530

    Add app config in a separate file

---
+++ src/config/app.js
@@ -0,0 +1,10 @@
+export const APP = {
+  NAME: "Inboxios",
+  VERSION: process.env.npm_package_version,
+  LINKS: {
+    GITHUB: "https://github.com/OzenFern/Inboxios",
+    DOCS: "https://github.com/OzenFern/Inboxios#readme",
+    TEMPLATE:
+      "https://ozenf.notion.site/cd94f0d949c1834aab81810764b9cd3c?v=0154f0d949c18313b1be08e4a4d3c1c7",
+  },
+};
--- src/config/locals.js
+++ src/config/locals.js
@@ -1,13 +1,7 @@
+import { APP } from "./app.js";
 import { ROUTES } from "./routes.js";
 
 export default function setLocals(app) {
   app.locals.APP = APP;
   app.locals.ROUTES = ROUTES;
 }
-
-const APP = {
-  NAME: "Inboxios",
-  VERSION: process.env.npm_package_version,
-  GITHUB: "https://github.com/OzenFern/Inboxios",
-  DOCS: "https://github.com/OzenFern/Inboxios#readme",
-};
