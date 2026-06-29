4e2fb895e2b12f548a60acd1b7fd72a888128b99
OzenFern <ozenfernandes@gmail.com>
2026-06-29 15:19:16 +0530

    Add status options as global variable

--- src/config/locals.js
+++ src/config/locals.js
@@ -1,7 +1,9 @@
 import { APP } from "./app.js";
 import { ROUTES } from "./routes.js";
+import { STATUS } from "./properties.js";
 
 export default function setLocals(app) {
   app.locals.APP = APP;
   app.locals.ROUTES = ROUTES;
+  app.locals.STATUS = STATUS;
 }
