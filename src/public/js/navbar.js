9d998437d41acfd64062e4b3d593b7e9d5832ad2
OzenFern <ozenfernandes@gmail.com>
2026-07-02 01:57:03 +0530

    Built responsive navbar

---
+++ src/public/js/navbar.js
@@ -0,0 +1,9 @@
+const navbarToggle = document.querySelector(".navbar-toggle");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+navbarToggle.addEventListener("click", () => {
+  navbarToggle.classList.toggle("active");
+  navbarMenu.classList.toggle("active");
+});
+
+// Write code to dynamically change the active page indicator
