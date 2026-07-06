6c8355d980ff4224667aa01cef4a8db96b20ce2c
OzenFern <ozenfernandes@gmail.com>
2026-07-06 20:07:39 +0530

    Use route parameter config

--- src/routes/error.js
+++ src/routes/error.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { ROUTE_PARAMS } from "../config/routeParams.js";
 import { showError } from "../controllers/errorController.js";
 
 const router = express.Router();
 
-router.get("/404", showError);
-router.get("/500", showError);
+router.get(`/:${ROUTE_PARAMS.ERROR}`, showError);
 
 export default router;
