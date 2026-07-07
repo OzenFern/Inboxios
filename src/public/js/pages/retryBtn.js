dc23dfff2bc5500ce074d4b8e8c49aeb39e96295
OzenFern <ozenfernandes@gmail.com>
2026-07-07 16:16:50 +0530

    Write the retry logic

---
+++ src/public/js/pages/retryBtn.js
@@ -0,0 +1,10 @@
+import { $ } from "../utils/selector.js";
+import { on } from "../utils/eventListener.js";
+
+on($("#retry-btn"), "click", () => {
+  if (history.length > 1) {
+    history.back();
+  } else {
+    window.location.href("/");
+  }
+});
