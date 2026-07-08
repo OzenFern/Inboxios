05d7fce7d90a8cd7511f7c0bd5d140f448c58ea4
OzenFern <ozenfernandes@gmail.com>
2026-07-08 20:31:50 +0530

    Move all utils in one file

--- src/public/js/utils.js
+++ src/public/js/utils.js
@@ -1,3 +1,8 @@
+// querySelectors
+export const $ = document.querySelector.bind(document);
+export const $$ = document.querySelectorAll.bind(document);
+
+// EventListeners
 export function on(element, eventType, callback) {
   if (typeof element.forEach === "function") {
     element.forEach((el) => el.addEventListener(eventType, callback));
