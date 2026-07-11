f262553fd1a1cf424eeec808b79055c746b80443
OzenFern <ozenfernandes@gmail.com>
2026-07-11 21:55:18 +0530

    Add Messages and QUERY PARAMS to global variables

--- src/config/locals.js
+++ src/config/locals.js
@@ -3,7 +3,7 @@ import { ROUTES } from "./routes.js";
 import { STATUS } from "./properties.js";
 import { formatDate } from "../utils/formatDate.js";
 import { BUTTON_LABELS } from "./labels.js";
-import { MESSAGES } from "./queryParams.js";
+import { MESSAGES, QUERY_PARAMS } from "./queryParams.js";
 
 export default function setLocals(app) {
   app.locals.APP = APP;
@@ -11,5 +11,6 @@ export default function setLocals(app) {
   app.locals.STATUS = STATUS;
   app.locals.BUTTON_LABELS = BUTTON_LABELS;
   app.locals.formatDate = formatDate;
-  app.locals.ERROR = MESSAGES.ERROR;
+  app.locals.MESSAGES = MESSAGES;
+  app.locals.QUERY_PARAMS = QUERY_PARAMS;
 }
