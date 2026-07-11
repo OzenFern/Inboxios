5f849975f829b3a05ac798e6859e6364fdca4015
OzenFern <ozenfernandes@gmail.com>
2026-07-11 17:03:17 +0530

    Update script to cache user filter preferences

---
+++ src/public/js/pages/tasksUtils/statusFilter.js
@@ -0,0 +1,34 @@
+import { $, on, storageCache } from "../../utils.js";
+
+const taskFilter = $("#status-filter");
+
+on(taskFilter, "change", async () => {
+  const filter = taskFilter.selectedOptions[0].value;
+  // Store filter value on change to localStorage
+  storageCache.set("task-filter", filter);
+
+  const params = new URLSearchParams({
+    status: filter,
+  });
+
+  location.search = filter ? params : "";
+});
+
+on(window, "load", () => {
+  const params = new URLSearchParams(location.search);
+
+  if (params.has("status")) {
+    taskFilter.value = params.get("status");
+    return;
+  }
+
+  const savedFilter = storageCache.get("task-filter");
+
+  if (!savedFilter) return;
+
+  const search = new URLSearchParams({
+    status: savedFilter,
+  });
+
+  location.search = search;
+});
