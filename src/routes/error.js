98b5e56c8fe8afff6d10fe0596305e8c0348070f
OzenFern <ozenfernandes@gmail.com>
2026-07-12 19:23:42 +0530

    Add weekly caching for error router

--- src/routes/error.js
+++ src/routes/error.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { ROUTE_PARAMS } from "../config/routeParams.js";
 import { showError } from "../controllers/errorController.js";
+import { cache } from "../middlewares/cache.js";
 
 const router = express.Router();
 
-router.get(`/:${ROUTE_PARAMS.ERROR}`, showError);
+router.get(`/:${ROUTE_PARAMS.ERROR}`, cache(604800), showError); // Cached for 1 week
 
 export default router;
